fdfe69b39b116cc2f39097603d013fb7afc7c6c0
Tobi <84527857+smarterToby@users.noreply.github.com>
2025-08-02 09:00:00 +0200

    👷 Add config for dev and release

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,37 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import securityPlugin from 'eslint-plugin-security'
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
+import tseslint from 'typescript-eslint'
+import {globalIgnores} from 'eslint/config'
+
+export default tseslint.config([globalIgnores(['dist']), {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs['recommended-latest'],
+        reactRefresh.configs.vite,
+        securityPlugin.configs.recommended,
+        {
+            plugins: {
+                prettier,
+            }, rules: {
+                'prettier/prettier': [
+                    1, {
+                        endOfLine: 'lf',
+                        printWidth: 120,
+                        semi: true, singleQuote: true,
+                        tabWidth: 2,
+                        trailingComma: 'es5',
+                    },],
+                'semi': ['error', 'always'],
+            },
+
+        },],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+        ecmaVersion: 2020, globals: globals.browser,
     },
-  },
-])
+},])
